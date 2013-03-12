5fffde5f62509f05099d973cfc7a2419a54c897b
John Wregglesworth <wregglej@iplantcollaborative.org>
2013-03-12 14:21:08 -0700

    Added dependencies. Got a basic webapp up and running with lein-ring.

---
+++ project.clj
@@ -0,0 +1,18 @@
+(defproject morbixon "0.1.0-SNAPSHOT"
+  :description "Logging service and provenance facade."
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [org.clojure/tools.logging "0.2.6"]
+                 [org.clojure/tools.cli "0.2.2"]
+                 [slingshot "0.10.3"]
+                 [compojure "1.1.5"]
+                 [ring/ring-jetty-adapter "1.1.8"]
+                 [ring/ring-devel "1.1.8"]
+                 [cheshire "5.0.2"]
+                 [com.cemerick/url "0.0.7"]
+                 [org.iplantc/clojure-commons "1.4.0-SNAPSHOT"]]
+  :ring {:handler morbixon.core/app}
+  :main morbixon.core
+  :plugins [[lein-ring "0.8.3"]
+            [org.iplantc/lein-iplant-rpm "1.4.0-SNAPSHOT"]]
+  :repositories {"iplant"
+                 "http://projects.iplantcollaborative.org/archiva/repository/internal/"})
---
+++ src/morbixon/core.clj
@@ -0,0 +1,59 @@
+(ns morbixon.core
+  (:use [compojure.core]
+        [morbixon.json-body :only [parse-json-body]]
+        [clojure-commons.query-params :only [wrap-query-params]]
+        [ring.middleware.params]
+        [ring.middleware.keyword-params]
+        [ring.middleware.nested-params]
+        [ring.middleware.multipart-params]
+        [ring.middleware.cookies]
+        [ring.middleware.session]
+        [ring.middleware.stacktrace]
+        [slingshot.slingshot :only [try+ throw+]])
+  (:require [clojure.tools.cli :as cli]
+            [compojure.route :as route]
+            [compojure.handler :as handler]
+            [clojure.tools.logging :as log]
+            [ring.adapter.jetty :as jetty]))
+
+(defroutes morbixon-routes
+  (GET "/" [] "Morbixon says hi."))
+
+(defn site-handler [routes]
+  (-> routes
+      parse-json-body
+      wrap-multipart-params
+      wrap-keyword-params
+      wrap-nested-params
+      wrap-query-params
+      wrap-stacktrace))
+
+(defn parse-args
+  [args]
+  (cli/cli
+   args
+   #_(["-c" "--config"
+       "Set the local config file to read from. Bypasses Zookeeper."
+       :default nil])
+   ["-h" "--help"
+    "Show the help."
+    :default false
+    :flag true]
+   ["-p" "--port"
+    "Set the port to listen on."
+    :default 31305
+    :parse-fn #(Integer. %)]))
+
+(def app
+  (site-handler morbixon-routes))
+
+(defn -main
+  [& args]
+
+  (let [[opts args help-str] (parse-args)]
+    (cond
+     (:help opts)
+     (do (println help-str)
+         (System/exit 0)))
+
+    (jetty/run-jetty app {:port (:port opts)})))
