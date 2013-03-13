e502ddb7144202988737aa4ceb5096b375f3c590
John Wregglesworth <wregglej@iplantcollaborative.org>
2013-03-13 10:34:21 -0700

    Fixed the path to the conf dir in project.clj.

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
   :iplant-rpm {:summary "morbixon"
                :dependencies ["iplant-service-config >= 0.1.0-5"]
                :config-files ["log4j.properties"]
-               :config-path ["conf"]}
+               :config-path "conf"}
   :main morbixon.core
   :plugins [[lein-ring "0.8.3"]
             [org.iplantc/lein-iplant-rpm "1.4.0-SNAPSHOT"]]
